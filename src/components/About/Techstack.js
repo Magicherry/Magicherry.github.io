b8157f7189c860e4407ae7c14a43500aca5413f6
ZYT <13681756546@139.com>
2025-02-23 17:31:22 -0500

    Add link and Change the Layout of About Tech&Tool Page

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -14,29 +14,47 @@ import { SiSpringboot, SiRedis, SiNextdotjs } from "react-icons/si";
 import { CgCPlusPlus } from "react-icons/cg"; // 正确的导入路径
 
 const techStackIcons = [
-  { icon: <CgCPlusPlus />, name: "C++" },
-  { icon: <DiJava />, name: "Java" },
-  { icon: <DiJavascript1 />, name: "JavaScript" },
-  { icon: <DiPython />, name: "Python" },
-  { icon: <DiNodejs />, name: "Node.js" },
-  { icon: <DiReact />, name: "React" },
-  { icon: <IoLogoVue />, name: "Vue.js" },
-  { icon: <SiSpringboot />, name: "Spring Boot" },
-  { icon: <DiMongodb />, name: "MongoDB" },
-  { icon: <SiNextdotjs />, name: "Next.js" },
-  { icon: <DiGit />, name: "Git" },
-  { icon: <SiRedis />, name: "Redis" },
+  { icon: <CgCPlusPlus />, name: "C++", link: "https://isocpp.org/" },
+  { icon: <DiJava />, name: "Java", link: "https://www.java.com/" },
+  { icon: <DiJavascript1 />, name: "JavaScript", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+  { icon: <DiPython />, name: "Python", link: "https://www.python.org/" },
+  { icon: <DiNodejs />, name: "Node.js", link: "https://nodejs.org/" },
+  { icon: <DiReact />, name: "React", link: "https://react.dev/" },
+  { icon: <IoLogoVue />, name: "Vue.js", link: "https://vuejs.org/" },
+  { icon: <SiSpringboot />, name: "Spring Boot", link: "https://spring.io/projects/spring-boot" },
+  { icon: <DiMongodb />, name: "MongoDB", link: "https://www.mongodb.com/" },
+  { icon: <SiNextdotjs />, name: "Next.js", link: "https://nextjs.org/" },
+  { icon: <DiGit />, name: "Git", link: "https://git-scm.com/" },
+  { icon: <SiRedis />, name: "Redis", link: "https://redis.io/" },
 ];
 
 function Techstack() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {techStackIcons.map((tech, index) => (
-        <Col key={index} xs={4} md={2} className="tech-icons">
-          {tech.icon}
-        </Col>
-      ))}
-    </Row>
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        {techStackIcons.map((tech, index) => (
+            <Col key={index}
+                 xs={4} md={2} style={{ padding: "10px" }}>
+              <a
+                  className="tech-icons"
+                  href={tech.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title={tech.name} // 悬停显示技能名称
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                    height: "100%",
+                    textDecoration: "none",
+                    color: "inherit"
+                  }}
+              >
+                {tech.icon}
+              </a>
+            </Col>
+        ))}
+      </Row>
   );
 }
 
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,34 +1,51 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
-  SiLinux,
-  SiWindows11,
-  SiJetbrains,
+    SiVisualstudiocode,
+    SiPostman,
+    SiSlack,
+    SiVercel,
+    SiMacos,
+    SiLinux,
+    SiWindows11,
+    SiJetbrains,
 } from "react-icons/si";
 
 const toolStackIcons = [
-  { icon: <SiWindows11 />, name: "Windows 11" },
-  { icon: <SiMacos />, name: "MacOS" },
-  { icon: <SiLinux />, name: "Linux" },
-  { icon: <SiVisualstudiocode />, name: "Visual Studio Code" },
-  { icon: <SiJetbrains />, name: "JetBrains" },
+    { icon: <SiWindows11 />, name: "Windows 11", link: "https://www.microsoft.com/en-us/windows" },
+    { icon: <SiMacos />, name: "MacOS", link: "https://www.apple.com/macos/" },
+    { icon: <SiLinux />, name: "Linux", link: "https://www.kernel.org/" },
+    { icon: <SiVisualstudiocode />, name: "Visual Studio Code", link: "https://code.visualstudio.com/" },
+    { icon: <SiJetbrains />, name: "JetBrains", link: "https://www.jetbrains.com/" },
 ];
 
 function Toolstack() {
-  return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {toolStackIcons.map((tool, index) => (
-        <Col key={index} xs={4} md={2} className="tech-icons">
-          {tool.icon}
-        </Col>
-      ))}
-    </Row>
-  );
+    return (
+        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+            {toolStackIcons.map((tool, index) => (
+                <Col key={index} xs={4} md={2} style={{ padding: "10px" }}>
+                    <a
+                        className="tech-icons"
+                        href={tool.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        title={tool.name} // 悬停时显示工具名称
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%",
+                            height: "100%",
+                            textDecoration: "none",
+                            color: "inherit"
+                        }}
+                    >
+                        {tool.icon}
+                    </a>
+                </Col>
+            ))}
+        </Row>
+    );
 }
 
 export default Toolstack;
