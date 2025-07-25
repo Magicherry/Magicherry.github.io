7570745399d84fd73e25f2f742ac49b95f5a26a4
ZYT <your_username@users.noreply.github.com>
2025-07-25 00:53:56 -0400

    NavBar's hamburger button is back.

--- src/components/MainFrame/Navbar.js
+++ src/components/MainFrame/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
 import { CgGitFork, CgFileDocument } from "react-icons/cg";
@@ -14,47 +14,24 @@ function NavBar() {
   const [isExpanded, setIsExpanded] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const [isNavbarHidden, setIsNavbarHidden] = useState(false);
-  const [lastScrollY, setLastScrollY] = useState(0);
+  const lastScrollYRef = useRef(0);
 
-  const controlNavbar = () => {
-    if (typeof window !== 'undefined') {
-      // Check if the user has scrolled to the bottom of the page
-      const isAtBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 10; // 10px buffer
+  useEffect(() => {
+    function handleScroll() {
+      const scrollY = window.scrollY;
+      const atBottom = window.innerHeight + scrollY >= document.body.offsetHeight - 10;
+      setIsScrolled(scrollY >= 20);
 
-      if (isAtBottom) {
-        setIsNavbarHidden(false); // Always show navbar at the bottom
-      } else if (window.scrollY > lastScrollY && window.scrollY > 80) { // if scroll down hide the navbar
+      // 滚动向下隐藏，滚动向上显示
+      if (atBottom) {
+        setIsNavbarHidden(false);
+      } else if (scrollY > lastScrollYRef.current && scrollY > 80) {
         setIsNavbarHidden(true);
-      } else { // if scroll up show the navbar
+      } else {
         setIsNavbarHidden(false);
       }
-      setLastScrollY(window.scrollY);
+      lastScrollYRef.current = scrollY;
     }
-  };
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('scroll', controlNavbar);
-
-      return () => {
-        window.removeEventListener('scroll', controlNavbar);
-      };
-    }
-  }, [lastScrollY]);
-
-  useEffect(() => {
-    if (isNavbarHidden) {
-      document.body.classList.add("bottom-nav-is-hidden");
-    } else {
-      document.body.classList.remove("bottom-nav-is-hidden");
-    }
-  }, [isNavbarHidden]);
-
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY >= 20);
-    };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
@@ -63,103 +40,101 @@ function NavBar() {
   const closeNavbar = () => setIsExpanded(false);
 
   return (
-    <>
-      <Navbar
-        expanded={isExpanded}
-        fixed="top"
-        expand="lg"
-        className={`${isScrolled ? "sticky" : "navbar"} ${isNavbarHidden ? "navbar-hidden" : ""}`}
-      >
-        <Container>
-          <Navbar.Brand href="/" className="brand-centered">
-            MAGICHERRY.
-          </Navbar.Brand>
+      <>
+        <Navbar
+            expanded={isExpanded}
+            fixed="top"
+            expand="lg"
+            className={`${isScrolled ? "sticky" : "navbar"} ${isNavbarHidden ? "navbar-hidden" : ""}`}
+            onToggle={setIsExpanded}
+        >
+          <Container>
+            <Navbar.Brand as={Link} to="/" onClick={closeNavbar}>
+              MAGICHERRY.
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav">
+              <span />
+              <span />
+              <span />
+            </Navbar.Toggle>
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ms-auto" defaultActiveKey="#home">
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="/" end onClick={closeNavbar}>
+                    <AiOutlineHome className="navbar-icon" /> HOME
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="/about" onClick={closeNavbar}>
+                    <AiOutlineUser className="navbar-icon" /> ABOUT
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="/experiences" onClick={closeNavbar}>
+                    <MdWorkOutline className="navbar-icon" /> EXPERIENCES
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="/project" onClick={closeNavbar}>
+                    <AiOutlineFundProjectionScreen className="navbar-icon" /> PROJECTS
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="/resume" onClick={closeNavbar}>
+                    <CgFileDocument className="navbar-icon" /> RESUME
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="fork-btn">
+                  <Button
+                      href="https://github.com/Magicherry/peronsalwebsite"
+                      target="_blank"
+                      className="fork-btn-inner"
+                  >
+                    <CgGitFork className="navbar-fork-icon" />
+                    <AiFillStar className="navbar-star-icon" />
+                  </Button>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setIsExpanded(!isExpanded)}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </Navbar.Toggle>
-
-          <Navbar.Collapse id="responsive-navbar-nav">
-            {/* This part is for desktop view */}
-            <Nav className="ms-auto d-none d-lg-flex" defaultActiveKey="#home">
-              <Nav.Item>
-                <Nav.Link as={NavLink} to="/" onClick={closeNavbar}>
-                  <AiOutlineHome className="navbar-icon" /> HOME
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link as={NavLink} to="/about" onClick={closeNavbar}>
-                  <AiOutlineUser className="navbar-icon" /> ABOUT
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link as={NavLink} to="/experiences" onClick={closeNavbar}>
-                  <MdWorkOutline className="navbar-icon" /> EXPERIENCES
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link as={NavLink} to="/project" onClick={closeNavbar}>
-                  <AiOutlineFundProjectionScreen className="navbar-icon" /> PROJECTS
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link as={NavLink} to="/resume" onClick={closeNavbar}>
-                  <CgFileDocument className="navbar-icon" /> RESUME
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item className="fork-btn">
-                <Button
-                  href="https://github.com/Magicherry/peronsalwebsite"
-                  target="_blank"
-                  className="fork-btn-inner"
-                >
-                  <CgGitFork className="navbar-fork-icon" />{" "}
-                  <AiFillStar className="navbar-star-icon" />
-                </Button>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-      {/* This part is for mobile view bottom bar */}
-      <div className={`d-lg-none bottom-nav-container ${isNavbarHidden ? "bottom-nav-hidden" : ""}`}>
-        <Nav className="bottom-nav">
-          <Nav.Item>
-            <Nav.Link as={NavLink} to="/" end onClick={closeNavbar}>
-              <AiOutlineHome />
-              <span>HOME</span>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={NavLink} to="/about" onClick={closeNavbar}>
-              <AiOutlineUser />
-              <span>ABOUT</span>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={NavLink} to="/experiences" onClick={closeNavbar}>
-              <MdWorkOutline />
-              <span>EXPERIENCES</span>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={NavLink} to="/project" onClick={closeNavbar}>
-              <AiOutlineFundProjectionScreen />
-              <span>PROJECTS</span>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={NavLink} to="/resume" onClick={closeNavbar}>
-              <CgFileDocument />
-              <span>RESUME</span>
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </div>
-    </>
+        {/* Bottom Tab Bar for Mobile */}
+        <div className={`d-lg-none bottom-nav-container ${isNavbarHidden ? "bottom-nav-hidden" : ""}`}>
+          <Nav className="bottom-nav">
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/" end>
+                <AiOutlineHome />
+                <span>HOME</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/about">
+                <AiOutlineUser />
+                <span>ABOUT</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/experiences">
+                <MdWorkOutline />
+                <span>EXPERIENCES</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/project">
+                <AiOutlineFundProjectionScreen />
+                <span>PROJECTS</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/resume">
+                <CgFileDocument />
+                <span>RESUME</span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+      </>
   );
 }
 
