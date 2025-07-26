02823df3fda67588ce345e3c5514c0f151cb32ae
ZYT <your_username@users.noreply.github.com>
2025-07-26 05:34:07 -0400

    Upgraded from React 17 to React 18.

--- src/components/MainFrame/Particle.js
+++ src/components/MainFrame/Particle.js
@@ -1,71 +1,81 @@
 import React from "react";
-import Particles from "react-tsparticles";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useState } from "react";
 import { loadFull } from "tsparticles";
 
 function Particle() {
-  const particlesInit = async (main) => {
-    await loadFull(main);
-  };
+  const [init, setInit] = useState(false);
 
-  return (
-    <Particles
-      id="tsparticles"
-      init={particlesInit}
-      options={{
-        fullScreen: {
-          enable: true,
-          zIndex: -1
-        },
-        particles: {
-          number: {
-            value: 220,
-            density: {
-              enable: true,
-              value_area: 1500,
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        options={{
+          fullScreen: {
+            enable: true,
+            zIndex: -1
+          },
+          particles: {
+            number: {
+              value: 220,
+              density: {
+                enable: true,
+                value_area: 1500,
+              },
             },
-          },
-          line_linked: {
-            enable: false,
-            opacity: 0.03,
-          },
-          move: {
-            direction: "right",
-            speed: 0.05,
-          },
-          size: {
-            value: 1,
-          },
-          opacity: {
-            value: {
-              min: 0.1,
-              max: 1
+            line_linked: {
+              enable: false,
+              opacity: 0.03,
             },
-            animation: {
-              enable: true,
-              speed: 1,
-              sync: false,
-              startValue: "random",
-              destroy: "none"
+            move: {
+              direction: "right",
+              speed: 0.05,
+            },
+            size: {
+              value: 1,
+            },
+            opacity: {
+              value: {
+                min: 0.1,
+                max: 1
+              },
+              animation: {
+                enable: true,
+                speed: 1,
+                sync: false,
+                startValue: "random",
+                destroy: "none"
+              }
             }
           },
-        },
-        interactivity: {
-          events: {
-            onclick: {
-              enable: true,
-              mode: "push",
+          interactivity: {
+            events: {
+              onclick: {
+                enable: true,
+                mode: "push",
+              },
+            },
+            modes: {
+              push: {
+                particles_nb: 1,
+              },
             },
           },
-          modes: {
-            push: {
-              particles_nb: 1,
-            },
-          },
-        },
-        retina_detect: true,
-      }}
-    />
-  );
+          retina_detect: true,
+        }}
+      />
+    );
+  }
+
+  return <></>;
 }
 
 export default Particle;
